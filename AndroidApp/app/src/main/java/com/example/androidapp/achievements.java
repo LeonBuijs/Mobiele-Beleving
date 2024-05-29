2364c0cb20baa666b1dae3bce40b84014d83905d
tiemenbr <tiemenbr@hotmail.nl>
2024-05-29 12:10:00 +0200

    remade the scores and achievement activities

--- AndroidApp/app/src/main/java/com/example/androidapp/achievements.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/achievements.java
@@ -1,9 +1,6 @@
 package com.example.androidapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class achievements extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_achievements);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void setMainScreen(View view){
-        startActivity(new Intent(this, MainActivity.class));
-    }
-    public void setScoreScreen(View view){
-        startActivity(new Intent(this, scoreview.class));
-    }
-    public void setAchievementScreen(View view){
-        startActivity(new Intent(this, achievements.class));
     }
 }
--- AndroidApp/app/src/main/java/com/example/androidapp/scoreview.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/scoreview.java
@@ -6,16 +6,20 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class scoreViewClass extends AppCompatActivity {
+public class scoreview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstancesState) {
         super.onCreate(savedInstancesState);
-        setContentView(R.layout.scoreview);
+        setContentView(R.layout.activity_scoreview);
         Spinner spinner = findViewById(R.id.dropdown_menu);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -38,5 +42,4 @@ public class scoreViewClass extends AppCompatActivity {
         //todo maak een layout voor de dropdown menu inplaats van de vooraf gemaakte simple_spinner_item.
         spinner.setAdapter(adapter);
     }
-
 }
