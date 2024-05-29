5f0b14cc3df38a70ba037565479d9803b4033207
Zehond <fsas.7seal@gmail.com>
2024-05-29 16:23:35 +0200

    First try on MQTT
    does not yet fully work

--- AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.androidapp;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,8 +37,64 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
 
+
+    }
+    public void TESTMQTTTEMP(View view) {
+
+        String topic = "MobieleBelevingA5";
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
+                        clientId);
+
+        try {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setUserName("MobieleBelevingA5");
+            options.setPassword("liefsSybeA5".toCharArray());
+
+            IMqttToken token = client.connect(options);
+            int qos = 0;
+            IMqttToken subtoken = client.subscribe(topic,qos);
+            subtoken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG,"subscribed");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG,"failed to subscribe");
+                }
+            });
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+
+            String test = "testtesttest";
+            byte[] encodeTest = new byte[0];
+            try {
+                encodeTest = test.getBytes("UTF-8");
+                MqttMessage message = new MqttMessage(encodeTest);
+                client.publish(topic, message);
+            } catch (UnsupportedEncodingException | MqttException e) {
+                e.printStackTrace();
+            }
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
     public void setMainScreen(View view){
     }
     public void setScoreScreen(View view){
@@ -32,4 +103,6 @@ public class MainActivity extends AppCompatActivity {
     public void setAchievementScreen(View view){
         startActivity(new Intent(this, Achievements.class));
     }
+
+
 }
