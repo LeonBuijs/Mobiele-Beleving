953006ffa7a757a8277a9491549551e4b0a5787b
Mohammed <m.lahlal@student.avans.nl>
2024-06-16 20:40:57 +0200

    Geprobeerd pdf te laten maken van scores
    ik heb alles gecomment

--- AndroidApp/app/src/main/java/com/example/androidapp/Achievements.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/Achievements.java
@@ -74,7 +74,7 @@ public class Achievements extends AppCompatActivity {
         achievements.add(new Achievement(R.drawable.nieuwsgierige_avonturier_4, getString(R.string.nieuwsgierige_avonturier), getString(R.string.je_hebt_de_app_verkend_nu_weet_je_waar_alles_te_vinden_is), false));
         achievements.add(new Achievement(R.drawable.consistente_smasher_5, getString(R.string.consistente_smasher), getString(R.string.je_bent_goed_op_weg_blijf_slaan_en_verbeteren), false));
         achievements.add(new Achievement(R.drawable.dagelijkse_kampioen_6, getString(R.string.dagelijkse_kampioen), getString(R.string.vandaag_ben_jij_de_kampioen_laat_je_vrienden_zien_wie_de_baas_is), false));
-        achievements.add(new Achievement(R.drawable.tien_op_rij_7, getString(R.string.tien_op_rij), getString(R.string.je_bent_niet_te_stoppen_wat_een_doorzettingsvermogen), false));
+        achievements.add(new Achievement(R.drawable.tien_op_rij_7, getString(R.string.tien_op_rij), getString(R.string.je_bent_niet_te_stoppen_wat_een_doorzettingsvermogen), true));
         achievements.add(new Achievement(R.drawable.magische_mepper_6, getString(R.string.magische_mepper), getString(R.string.wat_een_kracht_je_hebt_een_magische_slag_in_je), false));
         achievements.add(new Achievement(R.drawable.krachtpatser_9, getString(R.string.krachtpatser), getString(R.string.je_kracht_is_onge_venaard_houd_dit_vol), false));
         achievements.add(new Achievement(R.drawable.high_striker_meester_10, getString(R.string.high_striker_meester), getString(R.string.je_hebt_de_high_striker_onder_de_knie_fantastisch_werk), false));
@@ -84,7 +84,7 @@ public class Achievements extends AppCompatActivity {
         achievements.add(new Achievement(R.drawable.teamplayer_14, getString(R.string.teamplayer), getString(R.string.samen_slaan_is_altijd_leuker_bedankt_voor_het_uitnodigen_van_je_vrienden), false));
         achievements.add(new Achievement(R.drawable.social_star_15, getString(R.string.social_star), getString(R.string.je_scores_gaan_viral_deel_de_magie_van_johan_en_de_eenhoorn), false));
         achievements.add(new Achievement(R.drawable.earlybird_16, getString(R.string.early_bird), getString(R.string.vroeg_uit_de_veren_en_vol_energie_goed_gedaan), false));
-        achievements.add(new Achievement(R.drawable.night_owl_17, getString(R.string.night_owl), getString(R.string.nachtelijke_avonturier_je_energie_kent_geen_grenzen), false));
+        achievements.add(new Achievement(R.drawable.night_owl_17, getString(R.string.night_owl), getString(R.string.nachtelijke_avonturier_je_energie_kent_geen_grenzen), true));
         achievements.add(new Achievement(R.drawable.feestbeest_18, getString(R.string.feestbeest), getString(R.string.wat_een_feestelijke_slag_fijne_feestdag_en_blijf_genieten), true));
         achievements.add(new Achievement(R.drawable.uitdager_19, getString(R.string.uitdager), getString(R.string.je_hebt_je_vriend_verslagen_de_uitdaging_is_gewonnen), false));
         achievements.add(new Achievement(R.drawable.reisgenoot_20, getString(R.string.reisgenoot), getString(R.string.je_hebt_de_high_striker_op_verschillende_plekken_ervaren_wat_een_reis), true));
